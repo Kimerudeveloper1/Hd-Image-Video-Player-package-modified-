ee2d5bde924d84e96ffe0a1ba5288e13c664b265
Artem Petrenko <artem.petrenko@cactussoft.biz>
2018-06-19 17:08:44 +0300

    no message

--- node.lua
+++ node.lua
@@ -9,10 +9,12 @@ local shaders = {
         uniform vec4 Color;
         uniform float x, y, s;
         void main() {
-            vec2 texcoord = TexCoord * vec2(s, s) + vec2(x, y);
+            vec2 texcoord = TexCoord  vec2(s, s) + vec2(x, y);
             vec4 c1 = texture2D(Texture, texcoord);
-            vec4 c2 = texture2D(Texture, texcoord + vec2(0.0002, 0.0002));
-            gl_FragColor = (c2+c1)*0.5 * Color;
+            vec4 c2 = texture2D(Texture, texcoord + vec2(0.0008, 0.0000));
+            vec4 c3 = texture2D(Texture, texcoord + vec2(0.0004, 0.0004));
+            vec4 c4 = texture2D(Texture, texcoord + vec2(0.0000, 0.0008));
+            gl_FragColor = (c4+c3+c2+c1)*0.25  Color;
         }
     ]], 
     simple = resource.create_shader[[
